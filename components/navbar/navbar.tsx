0c571cb35ea391f350ba60c6ea060448f57b0c10
avez-mohammad <mohammad.avez@tabsquare.com>
2025-06-22 12:52:17 +0530

    updates

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,64 +1,69 @@
-import React from "react"
-import Menu from '@/public/assets/icons/menu.svg';
-import Search from '@/public/assets/icons/search.svg';
-import CartIcon from '@/public/assets/icons/cart.svg';
-import Heart from '@/public/assets/icons/heart.svg';
+import React from "react";
+import Menu from "@/public/assets/icons/menu.svg";
+import Search from "@/public/assets/icons/search.svg";
+import CartIcon from "@/public/assets/icons/cart.svg";
+import Heart from "@/public/assets/icons/heart.svg";
 import Link from "next/link";
 import Image from "next/image";
 import SearchBox from "./search-box";
 import Cart from "../cart/cart";
 
 export default function Navbar() {
-
   return (
     <>
-        <nav className="fixed w-full top-0 bg-white drop-shadow shadow-md p-3 h-[60px] lg:h-[80px]">
-            <div className="flex justify-between flex-wrap h-full items-center lg:hidden">
-                <div className="flex space-x-4">
-                    <Link href="">
-                        <Menu className="text-2xl"/>
-                    </Link>
-                    <span className="text-ellipsis">Myntra</span>
-                </div>
+      <nav className="fixed w-full top-0 bg-white drop-shadow shadow-md p-3 h-[60px] lg:h-[80px] z-10">
+        <div className="flex justify-between flex-wrap h-full items-center lg:hidden">
+          <div className="flex space-x-4">
+            <Link href="">
+              <Menu className="text-2xl" />
+            </Link>
+            <span className="text-ellipsis">Myntra</span>
+          </div>
 
-                <div className="flex space-x-3">
-                    <Link href="">
-                        <Search className="text-2xl"/>
-                    </Link>
-                    <Link href="">
-                        <Heart className="text-2xl"/>
-                    </Link>
-                    <Link href="">
-                        <CartIcon className="text-2xl"/>
-                    </Link>
-                </div>
-            </div>
+          <div className="flex space-x-3">
+            <Link href="">
+              <Search className="text-2xl" />
+            </Link>
+            <Link href="">
+              <Heart className="text-2xl" />
+            </Link>
+            <Link href="">
+              <CartIcon className="text-2xl" />
+            </Link>
+          </div>
+        </div>
 
-            <div className="hidden lg:flex h-full items-center justify-around">
-                <Image src="/assets/images/myntra.webp" alt={""} width={56} height={56} className="w-auto h-auto"/>
-                <ul className="flex space-x-10">
-                    <li>Men</li>
-                    <li>Women</li>
-                    <li>Kids</li>
-                </ul>
-                <SearchBox/>
-                <div className="flex space-x-5">
-                    <Link href="">
-                        <div className="flex flex-col justify-center items-center">
-                            <Heart className="text-2xl"/>
-                            <span className="">Wishlist</span>
-                        </div>
-                    </Link>
-                    <Link href="">
-                        <div className="flex flex-col justify-center items-center">
-                            <CartIcon className="text-2xl"/>
-                            <span className="">Bag</span>
-                        </div>
-                    </Link>
-                </div>
-            </div>
-        </nav>
-        <Cart/>
+        <div className="hidden lg:flex h-full items-center justify-around">
+          <Image
+            src="/assets/images/myntra.webp"
+            alt={""}
+            width={56}
+            height={56}
+            className="w-auto h-auto"
+          />
+          <ul className="flex space-x-10">
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+          </ul>
+          <SearchBox />
+          <div className="flex space-x-5">
+            <Link href="">
+              <div className="flex flex-col justify-center items-center">
+                <Heart className="text-2xl" />
+                <span className="">Wishlist</span>
+              </div>
+            </Link>
+            <Link href="">
+              <div className="flex flex-col justify-center items-center">
+                <CartIcon className="text-2xl" />
+                <span className="">Bag</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <Cart />
     </>
-  )
+  );
 }
--- components/product/product-carousel.tsx
+++ components/product/product-carousel.tsx
@@ -1,74 +1,66 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import Slider from 'react-slick'
-import ProductCard from './product-card'
-import Skeleton from 'react-loading-skeleton';
-import { Transition } from '@headlessui/react';
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import ProductCard from "./product-card";
+import Skeleton from "react-loading-skeleton";
+import { Transition } from "@headlessui/react";
 
 export default function ProductCarousel() {
+  // const [isClient, setIsClient] = useState<boolean>(false);
+  const [isShowing, setIsShowing] = useState(false);
+  // useEffect(()=>{
+  //     setIsClient(true)
+  // },[])
 
-    const [isClient, setIsClient] = useState<boolean>(false);
-    const [isShowing, setIsShowing] = useState(false)
-    useEffect(()=>{
-        setIsClient(true)
-    },[])
-
-    if(!isClient){
-        return (
-            <Skeleton className='h-40 lg:h-56'/>
-        )
-    }else {
-        return (
-            <>
-            <Slider
-                infinite
-                speed={2000}
-                cssEase='linear'
-                arrows={false}
-                autoplay
-                slidesToShow={7}
-                slidesToScroll={1}
-                useTransform={false}
-                responsive={[
-                    {
-                        breakpoint:1136,
-                        settings: {
-                            slidesToShow: 5
-                        }
-                    },
-                    {
-                        breakpoint:891,
-                        settings: {
-                            slidesToShow: 4
-                        }
-                    },
-                    {
-                        breakpoint:658,
-                        settings: {
-                            slidesToShow: 3
-                        }
-                    },
-                    {
-                        breakpoint:450,
-                        settings: {
-                            slidesToShow: 2
-                        }
-                    },
-                ]}
-                >
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-            </Slider>
-            </>
-        )
-    }
+  // if(!isClient){
+  //     return (
+  //         <Skeleton className='h-40 lg:h-56'/>
+  //     )
+  // }else {
+  return (
+    <>
+      <Slider
+        infinite
+        speed={2000}
+        cssEase="linear"
+        arrows={false}
+        autoplay
+        slidesToShow={7}
+        slidesToScroll={1}
+        useTransform={false}
+        responsive={[
+          {
+            breakpoint: 1136,
+            settings: {
+              slidesToShow: 5,
+            },
+          },
+          {
+            breakpoint: 891,
+            settings: {
+              slidesToShow: 4,
+            },
+          },
+          {
+            breakpoint: 658,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 450,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+        ]}
+      >
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </Slider>
+    </>
+  );
+  // }
 }
